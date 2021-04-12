b04f106b33f0ef97d279b46e5e965a82afd8d339
Junyoung <tom9744@gmail.com>
2021-04-12 12:38:41 +0900

    Udemy Course: Selection Sort

---
+++ Udemy/Sorting/Selection Sort.js
@@ -0,0 +1,26 @@
+function swap(array, indexA, indexB) {
+    const temp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = temp;
+}
+
+function selectionSort(array) {
+    // 1. 배열의 맨 앞부터 요소를 선택한다.
+    for (let i = 0; i < array.length; i++) {
+        let minValIndex = i;
+
+        // 2. 선택된 요소 이후에 존재하는 요소 중 최소값을 찾는다.
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] < array[minValIndex]) {
+                minValIndex = j;
+            }
+        }
+
+        if (minValIndex !== i) {
+            // 3. 선택한 요소와 최소값 요소를 바꾼다.
+            swap(array, i, minValIndex);
+        }
+    }
+    return array;
+}
+
