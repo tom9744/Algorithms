ae12409bdffd90df560fd3f954cdfc8ca53e6894
Junyoung <tom9744@gmail.com>
2021-04-26 17:42:07 +0900

    파이썬 알고리즘 인터뷰 12장 - 전화번호 문자 조합

---
+++ 파이썬 알고리즘 인터뷰/12장 그래프/전화번호 문자 조합.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        result = []
+        keys = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+        
+        def dfs(index=0, path=""):
+            # [NOTE] digits 길이만큼 path가 생성되지 않으면 함수가 종료된다.
+            if len(digits) == len(path):
+                result.append(path)
+                return
+        
+            # [EXAMPLE] idx가 1일 때, { '3': 'def' }에 대해 반복문이 실행되지만,
+            # 다음 idx가 2가 되어서 반복문이 실행되지 않고 함수가 종료된다. 
+            for idx in range(index, len(digits)):
+                for char in keys[digits[idx]]:
+                    dfs(idx + 1, path + char)
+                    
+        dfs()
+        
+        return result
